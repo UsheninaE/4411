f6a0cd2693449571be8a2539c1d59712c9551efc
Ekaterina <tara@bk.ru>
2023-07-09 11:37:55 +0500

    задача -найди максимум

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿    // Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+
+Console.Clear();
+Console.WriteLine("введите число - ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число - ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+if (firstNumber > secondNumber)
+    Console.WriteLine(firstNumber);
+else
+   Console.WriteLine(secondNumber);
+
+   
